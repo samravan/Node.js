4f994d3f42bea52cd79bf28c44890c9604405ad2
mas <mas.sam1368@gmail.com>
2021-01-16 20:14:54 +0330

    Ex3-web server

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,13 +2,40 @@
  * Exercise 3: Create an HTTP web server
  */
 
-var http = require('http');
+const http = require('http');
+const fs = require('fs');
 
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  if (req.url === '/') {
+    fs.readFile('index.html',
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      })
+  };
+
+  if (req.url === '/index.js') {
+    fs.readFile('index.js',
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.end(content);
+      })
+  };
+
+  if (req.url === '/style.css') {
+    fs.readFile('style.css',
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(content);
+      })
+  };
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000, () => {
+  console.log('Server is running on port 3000')
+}); // The server starts to listen on port 3000
