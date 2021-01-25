37b1c3b4707e6c11e7a371c5c93add173ad7508e
Amirhossein <ahshadravan@gmail.com>
2021-01-25 14:20:56 +0330

    Project is added

--- week1/homework/hackyourtemperature/server.js
+++ week1/homework/hackyourtemperature/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const fs = require('fs');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello from backend to frontend!')
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: false }));
+
+
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/weather', function (req, res) {
+   const cityName = req.body;
+   res.send(cityName)
 })
+
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 app.listen(3000);
