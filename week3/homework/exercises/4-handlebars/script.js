a76002beef7bab087001f6cfc3f48c6b4fcd8b76
mas <mas.sam1368@gmail.com>
2021-01-29 10:55:06 +0330

    Ex4

--- week3/homework/exercises/4-handlebars/script.js
+++ week3/homework/exercises/4-handlebars/script.js
@@ -9,21 +9,7 @@
  * Hints:
  * - Check the handlebars npm page for examples and documentation
  */
-
-
-function drawCard() {
-  // YOUR CODE GOES IN HERE
-}
-
-drawCard();
-
-/**
- * Given an array, return an element from it chosen at random
- */
-function getRandomElement(array) {
-  // YOUR CODE GOES IN HERE
-}
-
+const Handlebars = require('handlebars');
 const subjects = [
   'shark',
   'popcorn',
@@ -33,7 +19,6 @@ const subjects = [
   'toothbrush',
   'cannon',
 ];
-
 const punchlines = [
   'watch movie with',
   'spread some love',
@@ -43,3 +28,22 @@ const punchlines = [
   'achieve world piece',
   'help people learn programing',
 ];
+
+drawCard(subjects, punchlines);
+
+function drawCard(subjects, punchlines) {
+  const cardData = {
+    subject: getRandomElement(subjects),
+    punchline: getRandomElement(punchlines)
+  }
+
+  const card = "{{subject}} is great to {{punchline}}";
+  const template = Handlebars.compile(card);
+  const result = template(cardData);
+
+  console.log(result)
+}
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
