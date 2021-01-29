a9ea851b3762dcdb3b14a45aba78c05b75c872c8
mas <mas.sam1368@gmail.com>
2021-01-29 09:51:01 +0330

    Ex2

--- week3/homework/exercises/2-authentication/script.js
+++ week3/homework/exercises/2-authentication/script.js
@@ -8,8 +8,21 @@
  * Hints:
  * - for basic authentication the username and password need to be base64 encoded
  */
-function printBooks() {
+const fetch = require('node-fetch');
+
+async function printBooks() {
   // YOUR CODE GOES IN HERE
+  try {
+    const url = 'https://restapiabasicauthe-sandbox.mxapps.io/api/books';
+    const fetchData = await fetch(url, {
+      headers: { 'Authorization': 'Basic YWRtaW46aHZnWDhLbFZFYQ==' }
+    });
+    const data = await fetchData.json();
+    console.log(data);
+  }
+  catch (error) {
+    console.log(error);
+  }
 }
 
 printBooks();
